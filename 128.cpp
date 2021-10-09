a3a83bc8fe026f2ae534084cdb10e6109558541a
jiangjianan <jiangjianan@didiglobal.com>
2021-10-09 15:18:05 +0800

    1009

--- 128.cpp
+++ 128.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-
+        if(nums.empty()) return 0;
+        unorder_set<int> uset;
+        for(auto num: nums) uset.insert(num);
+        int maxLength = 0;
+        int curLength = 0;
+        for(auto num: uset){
+            if(!uset.count(num-1)){
+                int start = num;
+                while(uset.count(++num));
+                maxLength = max(maxLength, num - start);
+            }
+        }
+        return maxLength;
     }
 };
--- README.md
+++ README.md
@@ -5,5 +5,5 @@ Update every!
 *121. 买卖股票的最佳时机  
 *215. 数组中的第K个最大元素 ToDo
 
-*128. 最长连续序列 ToDo
+*128. 最长连续序列 https://leetcode-cn.com/problems/longest-consecutive-sequence/solution/ha-xi-zui-qing-xi-yi-dong-de-jiang-jie-c-xpnr/ ToDo 
 
